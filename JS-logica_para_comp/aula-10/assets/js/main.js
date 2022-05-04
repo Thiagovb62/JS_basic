512f523f28e252f67ae2bb5f26deb7c319481d35
Thiago Viana Bezerra <Thiagovb62@gmail.com>
2022-05-04 15:43:22 -0300

    update scripts with prettier.

--- JS-logica_para_comp/aula-10/assets/js/main.js
+++ JS-logica_para_comp/aula-10/assets/js/main.js
@@ -1,30 +1,51 @@
-const h1 = document.querySelector('.container h1');
+const h1 = document.querySelector(".container h1");
 const data = new Date();
 h1.innerHTML = data.toString();
-function getDiaNome (diaSemana){ 
-    const diasSemana = ['segunda','Terça','Quarta','Quinta','Sexta','Sabado',
-    'Domingo']   
-    return diasSemana[diaSemana] 
+function getDiaNome(diaSemana) {
+  const diasSemana = [
+    "segunda",
+    "Terça",
+    "Quarta",
+    "Quinta",
+    "Sexta",
+    "Sabado",
+    "Domingo",
+  ];
+  return diasSemana[diaSemana];
 }
 
-function getNomeMes (mes){ 
-    const meses = ['janeiro','Fevereiro','Março','Abril','Maio','Junho',
-    'Julho','Agosto','Setembro','Outubro','novembro','Dezembro']
-    return meses[mes]
+function getNomeMes(mes) {
+  const meses = [
+    "janeiro",
+    "Fevereiro",
+    "Março",
+    "Abril",
+    "Maio",
+    "Junho",
+    "Julho",
+    "Agosto",
+    "Setembro",
+    "Outubro",
+    "novembro",
+    "Dezembro",
+  ];
+  return meses[mes];
 }
 
 function criaData(data) {
-    const diaSemana = data.getDay();
-    const numMes = data.getMonth();
+  const diaSemana = data.getDay();
+  const numMes = data.getMonth();
 
-    const nomeDia = getDiaNome(diaSemana);
-    const nomeMes = getNomeMes(numMes);
-    
-    return ( ` ${nomeDia}, ${data.getDate()}` + ` de ${nomeMes}` +
-        ` de ${data.getFullYear()} ${data.getHours()}:${data.getMinutes()} `
-    );
-};
-h1.innerHTML = criaData(data) 
+  const nomeDia = getDiaNome(diaSemana);
+  const nomeMes = getNomeMes(numMes);
+
+  return (
+    ` ${nomeDia}, ${data.getDate()}` +
+    ` de ${nomeMes}` +
+    ` de ${data.getFullYear()} ${data.getHours()}:${data.getMinutes()} `
+  );
+}
+h1.innerHTML = criaData(data);
 /*const h1 = document.querySelector('.container h1');
 const data = new Date();
 const opcoes = {
