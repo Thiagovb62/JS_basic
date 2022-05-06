55f44f3464f5ac6e4b7f9250530d41dad94dd44c
Thiago Viana Bezerra <Thiagovb62@gmail.com>
2022-05-06 04:56:34 -0300

    exercicio NodeList.

--- JS-logica_para_comp/aula-16/index.js
+++ JS-logica_para_comp/aula-16/index.js
@@ -6,6 +6,7 @@ for (let valor of nome) {
 //For classico - Geralmente com iteraveis (array ou string)
 //For in - GRetorna indice ou chave (array, string ou objetos),
 //For of - retorna o valor em si (iteraveis,arrays ou string)
+
 nome.forEach(function(valor,indice,array) {
     console.log(valor,indice,array);
 })
